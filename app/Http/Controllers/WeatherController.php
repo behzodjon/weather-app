cb7c3ad440488962d435f2206721ffd69807cc89
Behzodjon <behzodjontuit1998@gmail.com>
2022-05-01 12:37:47 +0500

    fix: minor changes

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,15 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PullWeatherDataRequest;
-use App\Jobs\PullCurrentAndForecastWeatherData;
-use App\Jobs\PullHistoricalWeatherData;
-use App\Jobs\PullWeather;
 use App\Jobs\PullWeatherApiData;
-use Carbon\Carbon;
-use App\Models\City;
 use Illuminate\Http\Request;
 use App\Models\WeatherForecast;
-use App\Services\OpenWeatherService;
 
 class WeatherController extends Controller
 {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -564,13 +564,7 @@
                 <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </div>
-                <form action="{{url('api/weather/store')}}" method="POST">
-                    @csrf
-
-                    <input type="date" id="start" name="date" >
-                           <button type="submit" class="btn btn-primary">Submit</button>
-
-                </form>
+                
             </div>
         </div>
     </div>
